cd0a6c13a27e1d1946cf9158f1a43e68f1f90d5a
jayarathinam30 <jayarathinam30@gmail.com>
2025-06-14 13:40:48 +0530

    Changed dependency injection to use @Autowired annotation

--- src/main/java/com/example/ratelimiter/controller/RateLimiterController.java
+++ src/main/java/com/example/ratelimiter/controller/RateLimiterController.java
@@ -2,16 +2,16 @@ package com.example.ratelimiter.controller;
 
 import com.example.ratelimiter.constants.AppConstants;
 import com.example.ratelimiter.service.RateLimiterService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(AppConstants.API_BASE_PATH)
-@RequiredArgsConstructor
 public class RateLimiterController {
 
-    private final RateLimiterService rateLimiterService;
+    @Autowired
+    private RateLimiterService rateLimiterService;
 
     @GetMapping(AppConstants.TEST_ENDPOINT)
     public ResponseEntity<String> testRateLimit(@RequestHeader(AppConstants.USER_ID_HEADER) String userId) {
